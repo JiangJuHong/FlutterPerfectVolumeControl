8dffe878b915126aae11eff02a0cf3851eb88f2c
蒋具宏 <690717394@qq.com>
2021-04-05 13:57:28 +0800

    feat 增加空安全配置，增加 getVolume 和 setVolume接口

--- lib/perfect_volume_control.dart
+++ lib/perfect_volume_control.dart
@@ -1,14 +1,25 @@
-
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
 class PerfectVolumeControl {
-  static const MethodChannel _channel =
-      const MethodChannel('perfect_volume_control');
+  static const MethodChannel _channel = const MethodChannel('perfect_volume_control');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  /// 获得设备当前音量，此属性为 getVolume 的简单写法
+  /// Get the current volume of the device，This property is a simple way to write getvolume
+  static Future<double> get volume => getVolume();
+
+  /// 获得设备当前音量
+  /// Get the current volume of the device
+  static Future<double> getVolume() async {
+    return await _channel.invokeMethod('getVolume');
+  }
+
+  /// 根据[volume]设置设备音量，音量取值范围为 0.0 - 1.0
+  /// Set the device volume according to [volume], and the volume range is 0.0 - 1.0
+  static Future<void> setVolume(double volume) async {
+    assert(volume <= 0 && volume >= 1);
+    return await _channel.invokeMethod('setVolume', {volume});
   }
 }
